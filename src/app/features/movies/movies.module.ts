f37d32a253152830fc7b51f0ced15d8da2dedd02
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-10 17:47:30 +0300

    create movies module structure

--- src/app/features/movies/movies.module.ts
+++ src/app/features/movies/movies.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailsComponent } from './details/details.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailsComponent,
+    CreateComponent,
+    EditComponent
+  ],
   imports: [
     CommonModule
   ]
