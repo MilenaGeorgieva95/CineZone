2f96c661564032488ad12ad2fe58222878009fd8
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-13 14:16:58 +0300

    add TMDB base url to environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,10 @@
 export const environment = {
-    TMDB_API_KEY:'2a67095a19fe998d54c907983fd56f39',
-    TMDB_ACCESS_TOKEN:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTY3MDk1YTE5ZmU5OThkNTRjOTA3OTgzZmQ1NmYzOSIsIm5iZiI6MTc1MjE1OTcxOC4wMDEsInN1YiI6IjY4NmZkNWU1OTE5NTE3YWNlOWE5NzU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.flOFYbT7SyyOrDVUZYbHL_SpzzfpNdgvNq6uTMHH86s'
-    TMDB_BASE_URL:'https://api.themoviedb.org/3'
+  TMDB_API_KEY: '2a67095a19fe998d54c907983fd56f39',
+  TMDB_ACCESS_TOKEN:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTY3MDk1YTE5ZmU5OThkNTRjOTA3OTgzZmQ1NmYzOSIsIm5iZiI6MTc1MjE1OTcxOC4wMDEsInN1YiI6IjY4NmZkNWU1OTE5NTE3YWNlOWE5NzU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.flOFYbT7SyyOrDVUZYbHL_SpzzfpNdgvNq6uTMHH86s',
+  TMDB_BASE_URL: 'https://api.themoviedb.org/3',
+  TMDB_ENDPOINTS: {
+    movies: '/discover/movie',
+    qry: '?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_keywords=abc',
+  },
 };
