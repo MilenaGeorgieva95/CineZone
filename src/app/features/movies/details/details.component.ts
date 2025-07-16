76aea9c8d9d8cea8950299a8d23918ee30ac2d72
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-16 11:45:05 +0300

    fetch movie details based on movie id param

--- src/app/features/movies/details/details.component.ts
+++ src/app/features/movies/details/details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TMDBApiService } from '../services/tmdb-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { fullMovieDetails } from 'src/app/types/movie';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  movieId: string = '';
+  movieDetails = {} as fullMovieDetails;
 
+  constructor(
+    private tmdbApiService: TMDBApiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.movieId = params.get('movieId')!;
+      this.tmdbApiService
+        .fetchMovieById(this.movieId)
+        .subscribe((movie) => this.movieDetails=movie);
+    });
+  }
 }
+
+// this.route.queryParamMap.subscribe(params => {
+//   const page = params.get('page');
+// });
--- src/app/features/movies/services/tmdb-api.service.ts
+++ src/app/features/movies/services/tmdb-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MovieResults } from 'src/app/types/movie';
+import { fullMovieDetails, MovieResults } from 'src/app/types/movie';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
@@ -10,18 +10,22 @@ export class TMDBApiService {
   private readonly baseUrl: string = environment.TMDB_BASE_URL;
   private readonly accessToken: string = environment.TMDB_ACCESS_TOKEN;
   private endpoints = {
-    allMovies:`${this.baseUrl}/discover/movie`,
+    allMovies: `${this.baseUrl}/discover/movie`,
     popular: `${this.baseUrl}/movie/popular`,
     search: `${this.baseUrl}/search/movie`,
-    movieDetails: (id: number) => `${this.baseUrl}/movie/${id}`,
+    movieDetails: (id: string) => `${this.baseUrl}/movie/${id}`,
   };
 
   constructor(private http: HttpClient) {}
 
   fetchMovies() {
-   return this.http
-      .get<MovieResults>(this.endpoints.allMovies, {
-        headers: { Authorization: `Bearer ${this.accessToken}` },
-      })
+    return this.http.get<MovieResults>(this.endpoints.allMovies, {
+      headers: { Authorization: `Bearer ${this.accessToken}` },
+    });
+  }
+  fetchMovieById(id:string) {
+    return this.http.get<fullMovieDetails>(this.endpoints.movieDetails(id), {
+      headers: { Authorization: `Bearer ${this.accessToken}` },
+    });
   }
 }
