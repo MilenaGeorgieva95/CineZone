91f308d7cf17fce4514b296ff5dd5bb6c69fc9e0
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-19 01:12:03 +0300

    add paths to watchlists catalog buttons

--- src/app/features/watchlists/watchlists.module.ts
+++ src/app/features/watchlists/watchlists.module.ts
@@ -7,6 +7,7 @@ import { DetailsComponent } from './details/details.component';
 import { WatchlistsRoutingModule } from './watchlists-routing.module';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -21,7 +22,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     WatchlistsRoutingModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
 })
 export class WatchlistsModule { }
