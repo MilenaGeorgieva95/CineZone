5e6076f88b284b0af20bf633fbe2fc8518f11f09
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-19 01:01:07 +0300

    add buttons to watchlists catalog

--- src/app/features/watchlists/edit/edit.component.ts
+++ src/app/features/watchlists/edit/edit.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent {
-
+  invalidForm:boolean=false;
+  errorMsg:string=''
+  editWatchlist(form: NgForm) {}
+  cancelHandler(form: NgForm) {
+    form.setValue({ title: '', description: '' });
+  }
 }
