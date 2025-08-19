05cb9a848761093ee5ee3d8fcb60166cc56aa54e
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-19 12:59:04 +0300

    redirect if owner guard returns false or error

--- src/app/core/guards/is-owner.activate.ts
+++ src/app/core/guards/is-owner.activate.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate } from "@angular/router";
-import { map, Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
+import { catchError, map, Observable, of, tap } from "rxjs";
 import { UserService } from "src/app/features/user/user.service";
 import { WatchlistsService } from "src/app/features/watchlists/services/watchlists.service";
 
@@ -8,14 +8,23 @@ import { WatchlistsService } from "src/app/features/watchlists/services/watchlis
 @Injectable({ providedIn: 'root' })
 
 export class OwnerGuard implements CanActivate {
-  constructor(private userService: UserService, private watchlistsService:WatchlistsService) {}
+  constructor(private userService: UserService, private watchlistsService:WatchlistsService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     const watchlistId = route.params['watchlistId'];
     const userId = this.userService.userId;
 
-    return this.watchlistsService.getById(watchlistId).pipe(
-      map(watchlist=> watchlist.ownerId.objectId === userId)
+return this.watchlistsService.getById(watchlistId).pipe(
+      map(watchlist => (watchlist.ownerId?.objectId || watchlist.ownerId) === userId),
+      tap(isOwner => {
+        if (!isOwner) {
+          this.router.navigate(['/watchlists']); 
+        }
+      }),
+      catchError(() => {
+        this.router.navigate(['/watchlists']); 
+        return of(false);
+      })
     );
   }
 }
