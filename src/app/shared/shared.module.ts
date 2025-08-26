59f77a6a1ffbd52ac4dcc43be223468c26bd5466
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-26 12:52:57 +0300

    add elapsed time custom pipe to comments created at

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,10 +8,11 @@ import { EmailDirective } from './validators/email.directive';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, StarRatingPipe, EmailDirective,MovieCardComponent, LoaderComponent, PageNotFoundComponent],
+  declarations: [HeaderComponent, FooterComponent, StarRatingPipe, EmailDirective,MovieCardComponent, LoaderComponent, PageNotFoundComponent, ElapsedTimePipe],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent, StarRatingPipe, EmailDirective, MovieCardComponent, LoaderComponent, PageNotFoundComponent],
+  exports: [HeaderComponent, FooterComponent, StarRatingPipe, EmailDirective, MovieCardComponent, LoaderComponent, PageNotFoundComponent, ElapsedTimePipe],
 })
 export class SharedModule {}
