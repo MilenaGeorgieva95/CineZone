78e425bd506afc9c5b584dfe908e7eb0864247c7
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-09-04 16:02:33 +0300

    change comment details to edit component

--- src/app/features/comments/comments.module.ts
+++ src/app/features/comments/comments.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DetailsComponent } from './details/details.component';
+import { EditComponent,} from './edit/edit.component';
 
 
 
 @NgModule({
   declarations: [
-    DetailsComponent
+    EditComponent
   ],
   imports: [
     CommonModule
